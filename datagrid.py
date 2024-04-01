845ec7311b5f87fce54da919d21d6506ae65644c
siddhant250502 <79523568+siddhant250502@users.noreply.github.com>
2024-04-01 16:07:37 +0530

    Update datagrid.py

--- datagrid.py
+++ datagrid.py
@@ -228,7 +228,7 @@ if st.session_state.page == 0:
                         use_container_width=True
                         )
     st.session_state['file_path'] = res.loc[res.Select.idxmax()]['File Name']
-    st.session_state['upload_file'] = st.file_uploader("Upload your dataset", type=['csv'])
+    st.session_state['upload_file'] = st.file_uploader("Upload your dataset", type=['csv','xlsx'])
     
     if len(res[res.Select == True])==1 or st.session_state['upload_file'] is not None:
         col7, col8 = st.columns([0.1,1])
@@ -250,19 +250,18 @@ if st.session_state.page == 0:
 
 elif st.session_state.page == 1:
     st.button('Back to Datasets', on_click=back)
-    # st.write(st.session_state['file_path'][-3:])
-    # st.write((st.session_state['upload_file'].name)[-4:])
     with st.container(border=True):
         if (st.session_state['file_path'] is not None and st.session_state['file_path'][-3:]=="csv") or (st.session_state['upload_file'] is not None and (st.session_state['upload_file'].name)[-3:]=="csv"):
             if st.session_state['file_path'] is not None:
-                df = pd.read_csv(st.session_state['file_path'], delimiter=',')
+                try:
+                    df = pd.read_csv(st.session_state['file_path'], delimiter=',')
+                except:
+                    df = pd.read_csv(st.session_state['file_path'], delimiter=';')
             elif st.session_state['upload_file'] is not None:
-                df = pd.read_csv(st.session_state['upload_file'], delimiter=',')
-        elif (st.session_state['file_path'] is not None and st.session_state['file_path'][-4:]=="xlsx") or (st.session_state['upload_file'] is not None and (st.session_state['upload_file'].name)[-4:]=="xlsx"):
-            if st.session_state['file_path'] is not None:
-                df = pd.read_excel(st.session_state['file_path'])
-            elif st.session_state['upload_file'] is not None:
-                df = pd.read_excel(st.session_state['upload_file'])
+                try:
+                    df = pd.read_csv(st.session_state['file_path'], delimiter=',')
+                except:
+                    df = pd.read_csv(st.session_state['file_path'], delimiter=';')
         else:
             st.warning("File type Not supported")
         df = data_cleaning(df)
