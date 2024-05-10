15a558c5d590a226faf7de6b9b9df703c1c116e1
siddhant250502 <79523568+siddhant250502@users.noreply.github.com>
2024-05-10 12:06:51 +0530

    Update datagrid.py

--- datagrid.py
+++ datagrid.py
@@ -28,8 +28,8 @@ import kaleido
 
 
 st.set_page_config(page_title="AI Model Analysis",layout="wide")
-with open('style.css') as f:
-    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+# with open('style.css') as f:
+#     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 hide_streamlit_style = """
 <style>
 #MainMenu {
