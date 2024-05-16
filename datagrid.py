5aa49d4e1f64bb489e18435deb920a95c602c9aa
siddhant250502 <79523568+siddhant250502@users.noreply.github.com>
2024-05-16 18:18:36 +0530

    Update datagrid.py

--- datagrid.py
+++ datagrid.py
@@ -931,7 +931,8 @@ elif st.session_state.page == 1:
                                 labels[i] = 'samples = 0'
                         node.set('label', '<br/>'.join(labels))
                         node.set_fillcolor('white')
-                st.write('DT')
+                graph.write_png('tree.png')
+                st.image('tree.png')
                 # st.image(graph.create_png())
             # except:
             #     st.warning(f"Please run the AI model and the choose the Decision Tree Analysis")
