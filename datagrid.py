503972b52e30893704ddf2f478bc7f113417955e
siddhant250502 <79523568+siddhant250502@users.noreply.github.com>
2024-06-05 10:50:40 +0530

    Update datagrid.py

--- datagrid.py
+++ datagrid.py
@@ -925,11 +925,14 @@ elif st.session_state.page == 1:
             
         with t3:
             model = st.session_state['model']
-            dot_data = export_graphviz(model.estimators_[0], out_file=None,
+            try:
+                dot_data = export_graphviz(model.estimators_[0], out_file=None,
                                 feature_names=st.session_state['filter_df'].columns[:-1],
                                 class_names=[str(i) for i in st.session_state['filter_df'][st.session_state['filter_df'].columns[-1]].unique()],
                                 filled=True, rounded=True,
                                 special_characters=True)
+            except AttributeError:
+                pass
             dd_arr = []
             for i in range(len(dot_data)):
                 if dot_data[i:i+10] == 'fillcolor=':
